b45ae64b580f5d338375b5b55337cc9162f439d0
Eugenio <eugenio.sales@outlook.com>
2020-09-28 01:57:54 -0300

    Update select airports and build the graph of airports

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 // Api
 import { airportApi, url } from '../../services/airports';
 
+// Utils
+import { buildGraph } from '../../utils/buildGraph';
+
 // Style
 import { Form, FormGroup, Button, Input, Col, Row, Label } from 'reactstrap';
 import './styles.css';
@@ -10,6 +13,8 @@ import './styles.css';
 const Search = () => {
   
     const [airports, setAirports] = useState([]);
+    const [origin, setOrigin] = useState({});
+    const [destiny, setDestiny] = useState({});
 
     const fetch = async () => {
         const response = await airportApi.get(url);
@@ -18,7 +23,7 @@ const Search = () => {
 
     useEffect(() => { fetch() }, []);
 
-    console.log(airports);
+    let graph = buildGraph(airports);
 
     return (
         <div className="container">
@@ -27,12 +32,32 @@ const Search = () => {
                 <Row form >
                     <Col md={6}>
                     <FormGroup>
-                        <Input type="origin" name="origin" id="origin" placeholder="Origem..." />
+                        <Input onChange={(event) => setOrigin(event.target.value)} value={origin} type="select" name="origin" id="exampleSelect" placeholder="Origem..." >
+                            {
+                                airports.length && airports.map(airport => 
+                                    (
+                                        <option key={airport['Airport ID']} value={airport['City']}>
+                                            {`${airport['City']} (${ airport['Country']})`}
+                                        </option>
+                                    )
+                                )
+                            }
+                        </Input>
                     </FormGroup>
                     </Col>
                     <Col md={6}>
                     <FormGroup>
-                        <Input type="password" name="password" id="examplePassword" placeholder="Destino..." />
+                        <Input value={destiny} type="select" name="select" id="exampleSelect" placeholder="Destino..." >
+                            {
+                                airports.length && airports.map(airport => 
+                                    (
+                                        <option key={airport['Airport ID']} value={airport}>
+                                            {`${airport['City']} (${ airport['Country']})`}
+                                        </option>
+                                    )
+                                )
+                            }
+                        </Input>
                     </FormGroup>
                     </Col>
                 </Row>
---
+++ src/utils/buildGraph.js
@@ -0,0 +1,36 @@
+const distance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow((x1 - x2), 2) +((y1 - y2), 2));
+}
+
+const buildGraph = (airports) => {
+    let graph = {};
+    let hashAirport = {};
+
+    airports.forEach(airport => {
+        hashAirport[airport['Airport ID']] = {
+            ...airport
+        }
+    });
+
+    if(hashAirport[1]) {
+        airports.forEach(airport => {
+            airport['destinations'].forEach(destination => {
+                if(hashAirport[1]) {
+                    graph[airport['Airport ID']] = {
+                        ...graph[airport['Airport ID']],
+                        [destination]: distance(
+                            hashAirport[destination]?hashAirport[destination]['Latitude']:0,
+                            hashAirport[destination]?hashAirport[destination]['Longitude']: 0,
+                            airport['Latitude'],
+                            airport['Longitude'],
+                        )
+                    }
+                }
+            });
+        });
+    }
+    return graph;
+}
+
+export default buildGraph;
+export { buildGraph };
