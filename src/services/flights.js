b227c3644c0dc1fe59abb6ed8955f49ab65cb18c
Eugenio <eugenio.sales@outlook.com>
2020-10-02 21:52:24 -0300

    Fix deploy

--- src/services/flights.js
+++ src/services/flights.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://api.aviationstack.com/v1/flights?access_key=8c9289b0f290c08ffd40c8ce693f2d90';
+const url = `http://api.aviationstack.com/v1/flights?access_key=${process.env.REACT_APP_ACCESS_KEY_AVATION}`;
 
 const flightApi = axios.create('');
 
